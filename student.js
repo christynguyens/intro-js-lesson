9e50d5924998332f8ef9029c1094d021b3fd7b25
Nguyen <23-cnguyen@seaprep.org>
2023-04-07 12:54:26 -0700

    finished lesson 4: strings

--- student.js
+++ student.js
@@ -30,7 +30,7 @@ console.log(person + " is rich, this is " + rich + " because they have this much
 
 let student = "christy";
 console.log(student);
-student = ava;
+// student = ava;
 console.log(student);
 
 /*
@@ -39,24 +39,27 @@ console.log(student);
 // 1. Create a doubleNumber function that takes in 1 number parameter and returns double the value.
 
 function doubleNumber(num) {
+    console.log(num+num);
     return num+num;
 }
-console.log(num+num);
+
+doubleNumber(5);
 
 // 2. Create a squareNumber function that takes in 1 number parameter and returns the square
 function squareNumber(num) {
+    console.log(num*num);
     return num*num;
 }
-console.log(num*num);
+squareNumber(5);
 
 // 3. Create a productNumber function that takes in 2 number parameters and returns the product of the 2 numbers
 const productNumber = (num1, num2) => 
 {
+    console.log(num1, num2);
     return num1 * num2; 
 }
 
-console.log(productNumber(num1, num2));
-
+productNumber(2, 5);
 
 /*
 ========== LESSON 3 - NUMBERS ==========
@@ -85,3 +88,32 @@ function getRandom() {
   return Math.random() * (100-1) + 1;
 }
 
+/*
+========== LESSON 4 - STRINGS ==========
+*/
+// 1. Create a getPersonStats function statement that takes in 3 Strings: name, job, age. The function should console log a statement such as "Hi my name is [NAME], I am [AGE] years old, and I am a [JOB]".
+function getPersonStats(name, job, age) {
+    console.log("Hi my name is " + name + ", I am " + age + " years old, and I am a " + job);
+}
+
+getPersonStats("bryan", "doctor", 3);
+
+// 2. Create a sayFirstAndLast function expression that takes in 1 String parameter and console logs "the first character is: [firstChar] and last character is: [lastChar]".
+const sayFirstAndLast = function(str) {
+    var stri = str
+    console.log("the first character is: " + stri[0] + " and last character is: " + stri[stri.length -1]);
+}
+sayFirstAndLast("hello");
+
+// 3. Create a yellFruitNum arrow function that takes in 1 number parameter, 1 String that is a fruit parameter, and console logs the sentence "I have [number] [FRUIT]" in all caps.
+const yellFruitNum = (num, str) => console.log("I have " + num + " " + str.toUpperCase())
+yellFruitNum(5, "banana");
+// 4. Create a sliceFruit function that takes in a String of a fruit and console logs everything except the first 2 letters.
+function sliceFruit(str) {
+    console.log(str.substring(2));
+}
+sliceFruit("mango");
+// 5. Create a splitFruit function that takes in a String of a fruit and returns an array of the letters split into each index of the array.
+function splitFruit(str) {
+    return str.spliit(" ");
+}
